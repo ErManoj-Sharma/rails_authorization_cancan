29446e5769ead08365c15043bd19f550d8cd0f97
ErManoj-Sharma <manoj.sharma@ongraphmail.com>
2023-05-26 09:48:36 +0530

    added authenctication and authorization

--- Gemfile
+++ Gemfile
@@ -3,6 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.1'
 
+
+gem 'rails_admin'
+gem 'devise'
+gem 'cancancan'
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4'
 # Use sqlite3 as the database for Active Record
---
+++ app/models/ability.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.admin?
+
+    else
+      can :update, Question do |question|
+        question.user == user
+      end
+      can :destroy, Question do |question|
+        question.user == user
+      end
+
+      can :update, Answer do |answer|
+        answer.user == user
+      end
+      can :destroy, Answer do |answer|
+        answer.user ==   user
+      end
+
+      can :create, Question
+      can :create, Answer
+    end
+  end
+end
